810a2d0579994e4f2ecb1ddd73d5db434d0071ec
naf-mac <psypherpro@gmail.com>
2019-12-09 11:23:17 +0800

    Fixed indention

--- infra/ansible_structure.sh
+++ infra/ansible_structure.sh
@@ -17,8 +17,8 @@ touch production staging site.yml webservers.yml dbaservers.yml
 mkdir -p library/ module_utils/ filter_plugins/
 
 mkdir group_vars host_vars
-touch group_vars/group1.yml group_vars/group2.yml # here we assign variables to particular groups
-touch host_vars/hostname1.yml host_vars/hostname2.yml # here we assign variables to particular systems
+touch group_vars/group1.yml group_vars/group2.yml               # here we assign variables to particular groups
+touch host_vars/hostname1.yml host_vars/hostname2.yml           # here we assign variables to particular systems
 
 # common - this hierarchy represents a "role"
 # webtier - same king of structure as "common" was above, done for the webtier role
@@ -30,16 +30,16 @@ cd roles
 # lookup_plugins -  or other types of plugins, like lookup in this case
 mkdir -p common/{tasks,handlers,templates,files,vars,defaults,meta,library,module_utils,lookup_plugins}
 
-touch common/tasks/main.yml #  <-- tasks file can include smaller files if warranted
-touch common/handlers/main.yml #  <-- handlers file
+touch common/tasks/main.yml                                     #  <-- tasks file can include smaller files if warranted
+touch common/handlers/main.yml                                  #  <-- handlers file
 
 #  <-- files for use with the template resource
-touch common/templates/ntp.conf.j2  #  <------- templates end in .j2
+touch common/templates/ntp.conf.j2                              #  <------- templates end in .j2
 
 #  <-- files for use with the copy resource
 #  <-- script files for use with the script resource
 touch common/files/{bar.txt,foo.sh}
 
-touch common/vars/main.yml #  <-- variables associated with this role
-touch common/defaults/main.yml #  <-- default lower priority variables for this role
-touch common/meta/main.yml #  <-- role dependencies
+touch common/vars/main.yml                                      #  <-- variables associated with this role
+touch common/defaults/main.yml                                  #  <-- default lower priority variables for this role
+touch common/meta/main.yml                                      #  <-- role dependencies
